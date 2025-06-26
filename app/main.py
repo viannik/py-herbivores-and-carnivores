2376e86e2f5c15bde2e60dbe71cc564ce3120856
viannik <ivan.nikitski@gmail.com>
2025-06-26 18:03:41 +0300

    add Animal, Herbivore, and Carnivore classes with basic functionality

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+from dataclasses import dataclass
+
+
+@dataclass
+class Animal():
+    name: str
+    health: int = 100
+    hidden: bool = False
+
+    alive = []
+    def __post_init__(self):
+        Animal.alive.append(self)
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self):
+        self.hidden = not self.hidden
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Herbivore):
+        if not herbivore.hidden and isinstance(herbivore, Herbivore):
+            herbivore.health -= 50
+            if herbivore.health <= 0:
+                Animal.alive.remove(herbivore)
