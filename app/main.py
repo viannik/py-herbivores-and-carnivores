a774425c062fa9bb49c538853d9efab60e9db729
viannik <ivan.nikitski@gmail.com>
2025-06-26 18:11:30 +0300

    refactor Animal class and methods for improved type hints and readability

--- app/main.py
+++ app/main.py
@@ -2,25 +2,32 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Animal():
+class Animal:
     name: str
     health: int = 100
     hidden: bool = False
 
     alive = []
-    def __post_init__(self):
+
+    def __post_init__(self) -> None:
         Animal.alive.append(self)
 
-    def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
+
 class Carnivore(Animal):
-    def bite(self, herbivore: Herbivore):
+    @staticmethod
+    def bite(herbivore: Herbivore) -> None:
         if not herbivore.hidden and isinstance(herbivore, Herbivore):
             herbivore.health -= 50
             if herbivore.health <= 0:
